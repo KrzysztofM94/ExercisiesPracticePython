04f3fe3b4468a1290c4d53182b309ec68e1e1e8b
Kriss <geraltrivia19@gmail.com>
2020-03-03 17:11:08 +0100

    Exe 2: Extras: 1

--- Exe2.py
+++ Exe2.py
@@ -1,6 +1,16 @@
 # Main Solutiuon
+# number = int(input("Writing the number: "))
+# if (number % 2 == 0):
+#     print("{} is even.".format(number))
+# else:
+#     print("{} is odd.".format(number))
+
+# Extras: 1
 number = int(input("Writing the number: "))
 if (number % 2 == 0):
-    print("{} is even.".format(number))
+    if (number % 4 == 0):
+        print("{} is multiple 4.".format(number))
+    else:
+        print("{} is even.".format(number))
 else:
     print("{} is odd.".format(number))
