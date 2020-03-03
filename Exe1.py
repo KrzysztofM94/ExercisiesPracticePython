0eccc0bce7051c20133103bb0984784cc43a0685
Kriss <geraltrivia19@gmail.com>
2020-03-03 16:57:40 +0100

    Extras 2

--- Exe1.py
+++ Exe1.py
@@ -7,10 +7,17 @@ import datetime
 # print("{} in {} years you well be 100!!!".format(name, (hundred_year.year + (100 - age))))
 
 # Extras 1:
+# name = input("Write your name: ")
+# age = int(input("Write your age: "))
+# repeat = int(input("How many I will must repeat answer?"))
+# hundred_year = datetime.datetime.now()
+# message = "{} in {} years you well be 100!!! ".format(name, (hundred_year.year + (100 - age)))
+# print(message * repeat)
+
+# Extras 2:
 name = input("Write your name: ")
 age = int(input("Write your age: "))
 repeat = int(input("How many I will must repeat answer?"))
 hundred_year = datetime.datetime.now()
-message = "{} in {} years you well be 100!!! ".format(name, (hundred_year.year + (100 - age)))
+message = "{} in {} years you well be 100!!! \n".format(name, (hundred_year.year + (100 - age)))
 print(message * repeat)
-
