fe43c32794459f270ff388fb810f95b18e60f1c9
Kriss <geraltrivia19@gmail.com>
2020-03-03 17:19:38 +0100

    Exe 2: Extras: 2

--- Exe2.py
+++ Exe2.py
@@ -6,11 +6,20 @@
 #     print("{} is odd.".format(number))
 
 # Extras: 1
-number = int(input("Writing the number: "))
-if (number % 2 == 0):
-    if (number % 4 == 0):
-        print("{} is multiple 4.".format(number))
-    else:
-        print("{} is even.".format(number))
+# number = int(input("Writing the number: "))
+# if (number % 2 == 0):
+#     if (number % 4 == 0):
+#         print("{} is multiple 4.".format(number))
+#     else:
+#         print("{} is even.".format(number))
+# else:
+#     print("{} is odd.".format(number))
+
+# Extras: 2
+num = int(input("Please write first number: "))
+check = int(input("Please write second number: "))
+
+if(num%check==0):
+    print("{} number divides evenly into {} number".format(num,check))
 else:
-    print("{} is odd.".format(number))
+    print("{} number not divides evenly into {} number".format(num, check))
